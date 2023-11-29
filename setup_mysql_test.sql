a2fcca7e60f024761439836653e05bdb9f8eef0f
GuilleP2018 <gpereyo@gmail.com>
2023-11-29 13:41:10 -0400

    setup mysql test file upload

---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+-- Setup mysql test database
+
+-- Create database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
+-- Create user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- Grant privileges to user
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+-- Grant SELECT privileges to new user on 'performance_schema' database
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
