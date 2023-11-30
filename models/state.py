4eade93b75cfe1dc27d23c3fceb10f5f6668a89e
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-11-30 03:32:54 -0400

    update city and state added relitionships

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import os
+from models.base_model import BaseModel, Base
+import models
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
+from models import storage
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete", backref="state")
+
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Getter attribute cities that returns the list of City instances
+            with state_id equals to the current State.id"""
+            city_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
