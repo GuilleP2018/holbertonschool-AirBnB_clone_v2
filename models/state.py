47ede70933d3e0a6b5e301c68c13a0ce19f38609
GuilleP2018 <gpereyo@gmail.com>
2023-11-30 16:46:10 -0400

    state update and console fix

--- models/state.py
+++ models/state.py
@@ -12,15 +12,17 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
-
     if os.getenv('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def cities(self):
-            """Getter attribute cities that returns the list of City instances
-            with state_id equals to the current State.id"""
-            city_list = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+        cities = relationship("City", cascade="all, delete", backref="state")
+
+    else:
+        class State(BaseModel):
+            @property
+            def cities(self):
+                """Getter attribute cities that returns the list of City instances
+                with state_id equals to the current State.id"""
+                city_list = []
+                for city in models.storage.all(City).values():
+                    if city.state_id == self.id:
+                        city_list.append(city)
+                return city_list
