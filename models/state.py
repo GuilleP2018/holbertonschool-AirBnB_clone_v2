f0b4e5855c9aba775a31e1b0b5cdf6253020ea7b
GuilleP2018 <gpereyo@gmail.com>
2023-12-15 15:23:45 -0400

    changes to console

--- models/state.py
+++ models/state.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import os
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy.orm import Session
 from sqlalchemy.orm import relationship
-from models.city import City
+from sqlalchemy import Column, String
+from os import getenv
 import models
+from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
-        cities = relationship("City", cascade="all, delete", backref="state")
 
-    else:
-        class State(BaseModel):
-            @property
-            def cities(self):
-                """returns the list of City instances
-                with state_id equals to the current State.id"""
-                city_list = []
-                for city in models.storage.all(City).values():
-                    if city.state_id == self.id:
-                        city_list.append(city)
-                return city_list
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="all, delete")
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Returns the list of City instances with
+            state_id equals to the current State.id"""
+            from models import storage
+            all_cities = storage.all(City)
+            return [city for city in all_cities.values()
+                    if city.state_id == self.id]
+
+        def close(self):
+            """Close session"""
+            Session.close()
